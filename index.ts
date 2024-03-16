18471b97637732829eaa6e9ba6ff7b83a2047ac3
Felipe Martinez <felipeklez@gmail.com>
2024-03-16 19:12:54 -0300

    feat: add declarations and export types

--- index.ts
+++ index.ts
@@ -1,5 +1,9 @@
 import Instrumentation from './src/instrumentation';
+import Span from './src/opentelemetry/span';
+
+export * from './types/instrumentation.types';
 
 export {
   Instrumentation,
+  Span,
 };
--- test/index.spec.ts
+++ test/index.spec.ts
@@ -1,12 +1,22 @@
 import * as Index from '../index';
 import Instrumentation from '../src/instrumentation';
+import Span from '../src/opentelemetry/span';
+import * as Types from '../types/instrumentation.types';
 
 test('index exports', () => {
     expect(typeof Index.Instrumentation).toBe(typeof Instrumentation);
+    expect(typeof Index.Span).toBe(typeof Span);
+    expect(typeof Index.SpanArtifact).toBe(typeof Types.SpanArtifact);
+    expect(typeof Index.Spanner).toBe(typeof Types.Spanner);
+    expect(typeof Index.TELEMETRY).toBe(typeof Types.TELEMETRY);
 });
 
 test('index scope', () => {
     expect(Object.keys(Index).sort()).toEqual([
-        'Instrumentation'
+        'Instrumentation',
+        'Span',
+        'SpanArtifact',
+        'Spanner',
+        'TELEMETRY',
     ].sort())
 });
